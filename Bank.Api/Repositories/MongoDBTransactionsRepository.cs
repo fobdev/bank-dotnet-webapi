2c098ff42531ea674acff868eaa210ae2718de51
PedroDev <silvapedro55@gmail.com>
2021-10-24 11:33:38 -0300

    função movida

--- Bank.Api/Repositories/MongoDBTransactionsRepository.cs
+++ Bank.Api/Repositories/MongoDBTransactionsRepository.cs
@@ -19,17 +19,6 @@ namespace Bank.Api.Repositories
             transactionCollection = database.GetCollection<Transaction>(collectionName);
 
             this._user_repository = user_repository;
-
-        }
-        public void CreateTransaction(Transaction transaction)
-        {
-            User sender = _user_repository.GetUserById(transaction.sender);
-            User receiver = _user_repository.GetUserById(transaction.receiver);
-
-            _user_repository.SetUserBalance(sender, sender.balance - transaction.amount);
-            _user_repository.SetUserBalance(receiver, receiver.balance + transaction.amount);
-
-            transactionCollection.InsertOne(transaction);
         }
         public IEnumerable<Transaction> GetTransactions()
         {
@@ -40,21 +29,26 @@ namespace Bank.Api.Repositories
             var filter = Builders<Transaction>.Filter.Eq(user => user.id, id);
             return transactionCollection.Find(filter).SingleOrDefault();
         }
+        public void CreateTransaction(Transaction transaction)
+        {
+            _user_repository.SetUserBalance(transaction.receiver, transaction.amount, true);
+            _user_repository.SetUserBalance(transaction.sender, transaction.amount, false);
+
+            transactionCollection.InsertOne(transaction);
+        }
         public void RevertTransaction(Guid id)
         {
-            // data fetching
-            var transaction = GetTransactionById(id);
+            Transaction oldTransaction = GetTransactionById(id);
 
-            var userSender = _user_repository.GetUserById(transaction.sender);
-            var userReceiver = _user_repository.GetUserById(transaction.receiver);
-
-            // transaction operation
-            _user_repository.SetUserBalance(userSender, (userSender.balance + transaction.amount));
-            _user_repository.SetUserBalance(userReceiver, (userReceiver.balance - transaction.amount));
-
-            Transaction newTransaction = new()
+            CreateTransaction(new()
             {
                 id = Guid.NewGuid(),
+                sender = oldTransaction.receiver,
+                receiver = oldTransaction.sender,
+                amount = oldTransaction.amount,
+                createdAt = DateTimeOffset.UtcNow
+            });
+        }
         public bool CheckUserIsStaff(Guid id)
         {
             return _user_repository.GetUserById(id).staff;
