38122a0e3f027589b6b1d6085d3078e74fb9138c
PedroDev <silvapedro55@gmail.com>
2021-10-23 23:45:54 -0300

    testes criados

---
+++ Bank.UnitTests/TransactionControllerTests.cs
@@ -0,0 +1,106 @@
+using System;
+using Bank.Api.Controllers;
+using Bank.Api.Repositories;
+using Bank.Api.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+
+namespace Banking.UnitTests
+{
+    public class TransactionControllerTests
+    {
+
+        private readonly Mock<ITransactionRepository> transactionRepositoryStub = new();
+        private readonly Mock<IUserRepository> userRepositoryStub = new();
+        private readonly Random randomNumber = new();
+
+        [Fact]
+        public void GetTransactionById_WhenTransactionNotExists_ShouldReturnNotFound()
+        {
+            // Arrange
+            transactionRepositoryStub.Setup(repo => repo.GetTransactionById(It.IsAny<Guid>()))
+                .Returns((Transaction)null);
+
+            var controller = new TransactionController(transactionRepositoryStub.Object, userRepositoryStub.Object);
+
+            // Act
+            var result = controller.GetTransactionById(Guid.NewGuid());
+
+            // Assert
+            result.Result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public void GetTransactionById_WhenTransactionExists_ShouldReturnTransaction()
+        {
+            // Arrange
+            Transaction expectedItem = CreateRandomTransaction();
+
+            transactionRepositoryStub.Setup(repo => repo.GetTransactionById(It.IsAny<Guid>()))
+                .Returns(expectedItem);
+
+            var controller = new TransactionController(transactionRepositoryStub.Object, userRepositoryStub.Object);
+
+            // Act
+            var result = controller.GetTransactionById(Guid.NewGuid());
+
+            // Assert
+            result.Value.Should().BeEquivalentTo(expectedItem,
+            options => options.ComparingByMembers<Transaction>().ExcludingMissingMembers());
+        }
+
+        [Fact]
+        public void GetAllTransactions_WhenExists_ShouldReturnAllTransactions()
+        {
+            // Arrange
+            var expectedTransactions = new[] { CreateRandomTransaction(), CreateRandomTransaction(), CreateRandomTransaction() };
+
+            transactionRepositoryStub.Setup(repo => repo.GetTransactions())
+                .Returns(expectedTransactions);
+
+            var controller = new TransactionController(transactionRepositoryStub.Object, userRepositoryStub.Object);
+
+            // Act
+            var result = controller.GetAllTransactions();
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedTransactions,
+            options => options.ComparingByMembers<Transaction>().ExcludingMissingMembers());
+        }
+
+        public void CreateTransaction_WithTransactionToCreate_ShouldReturnCreatedTransaction()
+        {
+            // Arrange
+            var transactionToCreate = new TransactionCreateDto()
+            {
+                amount = randomNumber.NextDouble(),
+                receiver = Guid.NewGuid(),
+                sender = Guid.NewGuid(),
+            };
+
+            var controller = new TransactionController(transactionRepositoryStub.Object, userRepositoryStub.Object);
+
+            // Act
+            var result = controller.CreateTransaction(transactionToCreate);
+
+            // Assert
+
+
+        }
+
+        private Transaction CreateRandomTransaction()
+        {
+            return new()
+            {
+                id = Guid.NewGuid(),
+                receiver = Guid.NewGuid(),
+                sender = Guid.NewGuid(),
+                amount = randomNumber.NextDouble(),
+                createdAt = DateTimeOffset.UtcNow
+            };
+        }
+    }
+}
