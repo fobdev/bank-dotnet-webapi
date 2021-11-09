0fd674c292bfeebfa2fc4206be69361ef40343b7
fobdev <silvapedro55@gmail.com>
2021-11-09 18:12:52 -0300

    added requirement

--- Bank.Api/Dtos/BankCreateDto.cs
+++ Bank.Api/Dtos/BankCreateDto.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bank.Api.Dtos
 {
     public record PhysicalBankCreateDto
     {
+        [Required]
         public double balance { get; init; }
     }
 }
