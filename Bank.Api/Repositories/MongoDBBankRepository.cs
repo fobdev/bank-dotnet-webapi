3e9262792c607379b7623c217ce4a649ec24109d
fobdev <silvapedro55@gmail.com>
2021-11-09 17:24:42 -0300

    mongoDB repo added

---
+++ Bank.Api/Repositories/MongoDBBankRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using Bank.Api.Models;
+using MongoDB.Driver;
+
+namespace Bank.Api.Repositories
+{
+    class MongoDBBankRepository : IBankRepository
+    {
+        private const string databaseName = "bank";
+        private const string collectionName = "bank";
+        private readonly IMongoCollection<PhysicalBank> bankCollection;
+        public MongoDBBankRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            bankCollection = database.GetCollection<PhysicalBank>(collectionName);
+        }
+        public void CreateBank(PhysicalBank bank)
+        {
+            bankCollection.InsertOne(bank);
+        }
+
+        public PhysicalBank GetBank(Guid id)
+        {
+            var filter = Builders<PhysicalBank>.Filter.Eq(bank => bank.id, id);
+            return bankCollection.Find(filter).SingleOrDefault();
+        }
+
+        public void MakeLoan(Guid sending, Guid receiving, double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
